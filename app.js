d3fc53a312e94a1ca3140a0a869f1e636b6cee30
tibraj <tibraj94@gmail.com>
2020-12-29 12:19:19 -0500

    declaring ui elements

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ let min = 1,
     max = 10, 
     winningNumber = 2,
     guessesLeft = 3;
+
+const game = document.getElementById('#game'),
+      mimNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
