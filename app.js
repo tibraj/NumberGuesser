89cf00073135b7c5b2c84570239ddf93bf449111
tibraj <tibraj94@gmail.com>
2020-12-29 12:28:27 -0500

    event listener for guess

--- app.js
+++ app.js
@@ -3,9 +3,24 @@ let min = 1,
     winningNumber = 2,
     guessesLeft = 3;
 
-const game = document.getElementById('#game'),
+const game = document.querySelector('#game'),
       mimNum = document.querySelector('.min-num'),
       maxNum = document.querySelector('.max-num'),
       guessBtn = document.querySelector('#guess-btn'),
       guessInput = document.querySelector('#guess-input'),
       message = document.querySelector('.message');
+
+mimNum.textContent = min;
+maxNum.textContent = max;
+
+guessBtn.addEventListener('click', function(){
+    let guess = parseInt(guessInput.value);
+    if(isNaN(guess) || guess < min || guess > max){
+        setMessage(`Please Enter A Number Between ${min} and ${max}`, 'red');
+    }
+})
+
+function setMessage(msg, color) {
+    message.style.color = color;
+    message.textContent = msg
+}
